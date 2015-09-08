aaa5628807bceabfec25c3956e5263d3ec3020dc
silva <silva.haberl1989@gmail.com>
2015-09-08 15:00:41 +0200

    class Ruta

--- Metro/Metro/Models/Ruta.cs
+++ Metro/Metro/Models/Ruta.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.Data.Entity;
+
 namespace Metro.Models
 {
     public class Ruta
     {
+        public int Duljina { get; set; }
+        public string Start { get; set; }
+        public string Kraj { get; set; }
     }
+
+    public class RutaDBContext : DbContext
+    {
+        public DbSet<Ruta> Movies { get; set; }
+    }
+
 }
