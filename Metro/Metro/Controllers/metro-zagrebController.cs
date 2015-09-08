4c1ff463f4e1add9bc8141324dd3e9e545c2c485
silva <silva.haberl1989@gmail.com>
2015-09-08 13:02:42 +0200

    dodati metro-zagrebController

---
+++ Metro/Metro/Controllers/metro-zagrebController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Metro.Controllers
+{
+    public class metro_zagrebController : Controller
+    {
+        //
+        // GET: /metro-zagreb/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
