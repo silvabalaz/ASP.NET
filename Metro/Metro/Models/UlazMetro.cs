3526985a40cd63a1e4428f2bbfc9db6d15573c0b
SilvaHaberl <silva.haberl1989@gmail.com>
2015-09-17 21:36:13 +0200

    dodati klasu UlazMetro

---
+++ Metro/Metro/Models/UlazMetro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Metro.Models
+{
+    public class UlazMetro
+    {
+
+        //konstruktor
+        public UlazMetro()
+        {
+        
+            kvartovi = new List<Kvart>;
+            rute = new List<Ruta>;
+        
+        }
+
+        public UlazMetro Ime(string ime)
+        {
+            this.ime = ime;
+            return this;
+        }
+
+        public Metro getMetro()
+        {
+            return new Metro(kvartovi, rute, ime);
+        
+        }
+
+        protected IList<Kvart> kvartovi;
+        protected IList<Ruta> rute;
+
+        protected string ime;
+
+
+
+    }
+}
