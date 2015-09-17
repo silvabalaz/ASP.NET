b2711805ba4bd58986cc1e707399042850146cd8
SilvaHaberl <silva.haberl1989@gmail.com>
2015-09-17 18:18:22 +0200

    dodati klasu Put

---
+++ Metro/Metro/Models/Put.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Metro.Models
+{
+    public class Put
+    {
+        public Put(Metro metro)
+        {
+
+            MapaPuta = metro;
+        
+        }
+
+        public Metro MapaPuta { get; set; }
+
+        //METRO-2 za dani put vraca duljinu puta
+
+        public int? DuljinaPuta(string[] put) //nazivi kvartova kao stanica string[] put= {"MAKSIMIR","SIGET","SPANSKO"}
+        {
+            int duljina = 0;
+            char? trenutnaLokacija = null;
+
+            //za svaku rutu u putu
+
+            foreach (string kvart in put)
+            {
+                
+                if (trenutnaLokacija == null)
+                {
+                    trenutnaLokacija = kvart;
+                    continue;
+                
+                }
+          //pronađi zadanu rutu (put)
+          Ruta trazi =  
+              (from ruta in MapaPuta.Rute
+               where ruta.Start.KvartIme == trenutnaLokacija
+                    && ruta.Kraj.KvartIme == kvart
+                    orderby ruta.Duljina
+                    select ruta).DefaultIfEmpty(null).First();
+
+
+
+          if (trazi == null)
+          { 
+                // NO SUCH ROUTE
+              return null;
+          
+          }
+
+          duljina += trazi.Duljina;
+          trenutnaLokacija = kvart;
+
+            }
+
+
+            return duljina;
+        
+        
+        
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
