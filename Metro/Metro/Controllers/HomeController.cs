baa3a600422b8857f8ade8a2024cf64c8a7e2e2a
SilvaHaberl <silva.haberl1989@gmail.com>
2015-09-21 20:46:06 +0200

    upload file

--- Metro/Metro/Controllers/HomeController.cs
+++ Metro/Metro/Controllers/HomeController.cs
@@ -18,12 +18,33 @@ namespace Metro.Controllers
             return View("MyHomePage");
         }
 
+        [HttpPost]
         public ActionResult Pogled2()
         {
+            ViewBag.Message = "Isprobavam  ViewBag.Message";
             var serial ="SILVA";
-            return Json(new { name = "SILVA", value = serial}, JsonRequestBehavior.AllowGet);
+            //return Json(new { name = "SILVA", value = serial}, JsonRequestBehavior.AllowGet);
+            return View();
+        
         }
 
+
+        // This action handles the form POST and the upload
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Index");
+        }
 
 
     }
