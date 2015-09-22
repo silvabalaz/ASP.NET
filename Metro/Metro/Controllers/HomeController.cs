e74b22d5886315a149c7f99193dff3cb2fb23b94
SilvaHaberl <silva.haberl1989@gmail.com>
2015-09-22 10:56:18 +0200

    System.IO

--- Metro/Metro/Controllers/HomeController.cs
+++ Metro/Metro/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+
 
 namespace Metro.Controllers
 {
     public class HomeController : Controller
-    {
+    {  
         public ActionResult Index()
         {
             return View();
-        }
+        } 
 
         public ActionResult Pogled1()
         {
