0bc5b7d2d164a91ba5d9eb1b28a052f05a1fe88b
SilvaHaberl <silva.haberl1989@gmail.com>
2015-09-26 14:45:33 +0200

    promjena

--- Metro/Metro/Models/Put.cs
+++ Metro/Metro/Models/Put.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-/*
+
+
 namespace Metro.Models
 {
     public class Put
@@ -18,25 +19,74 @@ namespace Metro.Models
 
         public Metro MapaPuta { get; set; }
 
+        //Rekurzivna funkcija
+        public List<string> Stanice(Kvart start, int maxStanica, bool tocanBrojStanica)
+        {
+            List<string> popisStanica= new List<string>();
+
+            if (maxStanica == 0)
+            {
+                popisStanica.Add(start.ToString());
+            
+            }
+
+            Ruta[] susjedni = MapaPuta.SusjedniKvartovi(start);
+            
+            foreach(Ruta ruta in susjedni)
+            {
+                List<string> putevi = Stanice(ruta.Kraj, maxStanica-1,tocanBrojStanica);
+            
+                // za svaki put oznacimo da smo ga prosli
+
+                foreach (string put in putevi)
+                {
+                    putevi.Add(start.KvartIme + "-" + put);
+                }
+
+                if (!tocanBrojStanica)
+                {
+                    Stanice.Add(ruta.Start.KvartIme.ToString());
+                }
+
+            }
+                return Stanice;
+            
+            }
+
+        
+        
+        
+        
+        }
+
+
         //METRO-2 za dani put vraca duljinu puta
 
         public int? DuljinaPuta(string[] put) //nazivi kvartova kao stanica string[] put= {"MAKSIMIR","SIGET","SPANSKO"}
         {
             int duljina = 0;
-            char? trenutnaLokacija = null;
+            string trenutnaLokacija = null;
+
+
 
             //za svaku rutu u putu
 
             foreach (string kvart in put)
             {
-                
+                //preskoci
+                if(kvart == '-' )
+                continue;
+
+
                 if (trenutnaLokacija == null)
                 {
                     trenutnaLokacija = kvart;
                     continue;
                 
                 }
+
           //pronađi zadanu rutu (put)
+
           Ruta trazi =  
               (from ruta in MapaPuta.Rute
                where ruta.Start.KvartIme == trenutnaLokacija
@@ -89,4 +139,3 @@ namespace Metro.Models
     }
 }
 
-*/
