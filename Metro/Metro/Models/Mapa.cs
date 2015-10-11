654606b839cd0f3f265ce01124d5362d9397c0db
SilvaHaberl <silva.haberl1989@gmail.com>
2015-10-11 07:11:26 +0200

    promjena u ispisu ruta

--- Metro/Metro/Models/Mapa.cs
+++ Metro/Metro/Models/Mapa.cs
@@ -28,22 +28,17 @@ namespace Metro.Models
                 {
 
                     var results = dataItem.Split('-');
-
                     string a = results[0];
-                    string a1 = a.ToLower();
-                    string b = results[1];
-                    var dot = b.Split(':');
+                    string b = results[1]; 
+                    var dot = b.Split(':'); 
                     string c = dot[0];
                     var d = dot[1];
-                    string c2 = c[0] + c.Substring(1).ToLower();
-                    char[] s = a1.ToCharArray();
-                    s[0] = char.ToUpper(s[0]);
-                    string a2 = new string(s);
                     int duljina = Int32.Parse(d);
-                    Kvart kvart1 = new Kvart(a2);
-                    Kvart kvart2 = new Kvart(c2);
 
-                 
+                    Kvart kvart1 = new Kvart(a);
+                    Kvart kvart2 = new Kvart(c);
+                  
+                  
 
                     Ruta temp = new Ruta(kvart1, kvart2, duljina);
                     Rute.Add(temp);
